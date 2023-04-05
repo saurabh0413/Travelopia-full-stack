727c5b30ae3a3333a0a24eeeae27419d0b0f95f9
saurabh <saurabhfarkade83@gmail.com>
2023-04-06 03:16:28 +0530

    table-css-added

--- Frontend/travelapp/src/Pages/TravelDetails.jsx
+++ Frontend/travelapp/src/Pages/TravelDetails.jsx
@@ -1,8 +1,33 @@
-import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td, Text } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Navbar from "../Components/Navbar";
+import styled from "styled-components";
+const StyledTable = styled(Table)`
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
+  font-size: 14px;
 
+  th,
+  td {
+    text-align: center;
+    padding: 12px;
+    border: 1px solid #ddd;
+  }
+
+  th {
+    background-color: #f2f2f2;
+  }
+
+  tbody tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+  tbody tr:hover {
+    background-color: #ddd;
+  }
+`;
 export const TravelDetails = () => {
   const [traveldata, setTravelData] = useState([]);
 
@@ -21,30 +46,32 @@ export const TravelDetails = () => {
   return (
     <>
       <Navbar />
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Sr.No</Th>
-            <Th>Name</Th>
-            <Th>Email</Th>
-            <Th>Location</Th>
-            <Th>No.of.Travellers</Th>
-            <Th>Budget</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {traveldata.map((item, index) => (
-            <Tr key={index}>
-              <Td>{index+1}</Td>
-              <Td>{item.Name}</Td>
-              <Td>{item.Email}</Td>
-              <Td>{item.Location}</Td>
-              <Td>{item.No_of_travellers}</Td>
-              <Td>{item.Budget}</Td>
+      <StyledTable>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Sr.No</Th>
+              <Th>Name</Th>
+              <Th>Email</Th>
+              <Th>Location</Th>
+              <Th>No.of.Travellers</Th>
+              <Th>Budget</Th>
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
+          </Thead>
+          <Tbody>
+            { traveldata ? traveldata.map((item, index) => (
+              <Tr key={index}>
+                <Td>{index + 1}</Td>
+                <Td>{item.Name}</Td>
+                <Td>{item.Email}</Td>
+                <Td>{item.Location}</Td>
+                <Td>{item.No_of_travellers}</Td>
+                <Td>{item.Budget}</Td>
+              </Tr>
+            )) : <Text>No data available</Text>}
+          </Tbody>
+        </Table>
+      </StyledTable>
     </>
   );
 };
